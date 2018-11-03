f9d24cd2226dcdc22de554f6d20a67fb9d20e764
kelcok22 <kelcok22@live.co.uk>
2018-11-03 23:07:23 +0000

    added a base code for henchmen

---
+++ src/main/java/Kamen_Rider_Craft_4TH/mobs/Henchmen/Entityguardian.java
@@ -0,0 +1,78 @@
+package Kamen_Rider_Craft_4TH.mobs.Henchmen;
+
+
+import java.util.Calendar;
+import java.util.UUID;
+
+import Kamen_Rider_Craft_4TH.RiderItems;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIBreakDoor;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.RangedAttribute;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.pathfinding.PathNavigateGround;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class Entityguardian extends Entity_base_henchmen
+{
+	public Entityguardian(World par1World)
+	{
+		super(par1World);
+	}
+	public void onDeath(DamageSource cause)
+	{
+		if (!this.world.isRemote){
+			this.dropItem(RiderItems.full_bottle, 2);
+			switch (this.rand.nextInt(25))
+			{
+			case 0:
+				this.dropItem(RiderItems.rabbit_full_bottle, 1);
+				break;
+			case 1:
+				this.dropItem(RiderItems.tank_full_bottle, 1);
+				break;
+			case 2:
+				this.dropItem(RiderItems.gorilla_full_bottle, 1);
+				break;
+			case 3:
+				this.dropItem(RiderItems.diamond_full_bottle, 1); 
+				break;
+			case 4:
+				this.dropItem(RiderItems.ninjya_full_bottle, 1);
+				break;
+			case 5:
+				this.dropItem(RiderItems.comic_full_bottle, 1);
+				break;
+			case 6:
+				this.dropItem(RiderItems.taka_full_bottle, 1);
+				break;
+
+			case 7:
+				this.dropItem(RiderItems.gatling_full_bottle, 1);
+				break;
+			}
+		}
+	}
+}
---
+++ src/main/java/Kamen_Rider_Craft_4TH/mobs/Henchmen/Entityshocker_combatman.java
@@ -0,0 +1,60 @@
+package Kamen_Rider_Craft_4TH.mobs.Henchmen;
+
+import Kamen_Rider_Craft_4TH.RiderItems;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+
+public class Entityshocker_combatman extends Entity_base_henchmen
+{
+	public Entityshocker_combatman(World par1World)
+	{
+		super(par1World);
+	}
+
+	public void onDeath(DamageSource cause)
+	{
+		if (!this.world.isRemote){   
+
+			this.dropItem(RiderItems.rider_circuit, 3);
+			switch (this.rand.nextInt(25))
+			{   		   	    		
+			case 0:
+				this.dropItem(RiderItems.typhoon_core, 1);
+				break;
+
+			case 1:
+				this.dropItem(RiderItems.riderman_belt_core, 1);
+				break;
+
+			case 2:
+				this.dropItem(RiderItems.ridol_core, 1);
+				break;
+
+			case 3:
+				this.dropItem(RiderItems.condorer_wheel, 1);
+				break;
+
+			case 4:
+				this.dropItem(RiderItems.electrer_core, 1);
+				break;
+
+			case 5:
+				this.dropItem(RiderItems.tornado_core, 1);
+				break;
+
+			case 6:
+				this.dropItem(RiderItems.cyclode_core, 1);
+				break;
+
+			case 7:
+				this.dropItem(RiderItems.zx_belt_core, 1);
+				break;
+
+			case 8:
+				this.dropItem(RiderItems.king_stone_core, 1);
+				break;
+
+			}
+		}
+	}
+}
