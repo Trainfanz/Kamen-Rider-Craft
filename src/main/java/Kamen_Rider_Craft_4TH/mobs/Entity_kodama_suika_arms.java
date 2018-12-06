469551c4c45ba802ac4175814d923033cc2bbf02
kelcok22 <kelcok22@live.co.uk>
2018-12-06 01:21:31 +0000

    Added Kodama Suika Arms

---
+++ src/main/java/Kamen_Rider_Craft_4TH/mobs/Entity_kodama_suika_arms.java
@@ -0,0 +1,84 @@
+package Kamen_Rider_Craft_4TH.mobs;
+
+import Kamen_Rider_Craft_4TH.RiderItems;
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.world.World;
+
+public class Entity_kodama_suika_arms extends EntityAnimal
+{
+	public float field_70886_e;
+	public float destPos;
+	public float field_70884_g;
+	public float field_70888_h;
+	public float field_70889_i = 1.0F;
+
+	/** The time until the next egg is spawned. */
+	public int timeUntilNextEgg;
+
+	public Entity_kodama_suika_arms(World par1World)
+	{
+		super(par1World);
+		this.setSize(2F, 0.7F);
+		this.timeUntilNextEgg = this.rand.nextInt(6000) + 6000;
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
+		this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+		this.tasks.addTask(4, new EntityAIFollowParent(this, 1.1D));
+		this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		this.tasks.addTask(7, new EntityAILookIdle(this));
+	}
+
+	/**
+	 * Returns true if the newer Entity AI code should be run
+	 */
+	public boolean isAIEnabled()
+	{
+		return true;
+	}
+
+	protected void applyEntityAttributes()
+	{
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.4D);
+
+	}
+	
+	protected boolean shouldBurnInDay()
+	{
+		return false;
+	}
+	public void onDeath(DamageSource cause)
+	{
+		if (!this.world.isRemote){
+			this.dropItem(RiderItems.suika_lockseed, 1);
+		}
+	}
+
+	@Override
+	public EntityAgeable createChild(EntityAgeable entityageable) {
+
+		return null;
+	}
+
+
+
+}
