6c2ec33bcc64cfa7229e3bcabda08f41b3d16499
Nicholas Boos <trainfanzmail@gmail.com>
2018-12-08 20:01:25 -0500

    Pandora box can now give panels

--- src/main/java/Kamen_Rider_Craft_4TH/blocks/pandora_box_block.java
+++ src/main/java/Kamen_Rider_Craft_4TH/blocks/pandora_box_block.java
@@ -6,9 +6,11 @@ import Kamen_Rider_Craft_4TH.util.IHasModel;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
@@ -36,6 +38,11 @@ public class pandora_box_block extends machine_block  {
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
     	if (!worldIn.isRemote) {
     	     if (player.getHeldItem(hand).getItem() == RiderItems.full_bottle) process(player, worldIn, pos, hand, RiderItems.Pandora_bottle);
+    	     if (player.getHeldItem(hand).getItem() == Items.DYE&player.getHeldItem(hand).getMetadata()==2) process(player, worldIn, pos, hand, RiderItems.pandora_panel_green);
+    	     if (player.getHeldItem(hand).getItem() == Items.DYE&player.getHeldItem(hand).getMetadata()==4) process(player, worldIn, pos, hand, RiderItems.pandora_panel_blue);
+    	     if (player.getHeldItem(hand).getItem() == Items.DYE&player.getHeldItem(hand).getMetadata()==1) process(player, worldIn, pos, hand, RiderItems.pandora_panel_red);
+    	     if (player.getHeldItem(hand).getItem() == Items.DYE&player.getHeldItem(hand).getMetadata()==0) process(player, worldIn, pos, hand, RiderItems.pandora_panel_black);
+    	     if (player.getHeldItem(hand).getItem() == Items.DYE&player.getHeldItem(hand).getMetadata()==15) process(player, worldIn, pos, hand, RiderItems.pandora_panel_white);
     	    return true;
 	    }
         return false;
