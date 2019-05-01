fec16380602a5b38e68d83088f2fe906a2ba571a
kelcok22 <kelcok22@live.co.uk>
2019-05-01 23:32:57 +0100

    Update Item_full_bottle.java

--- src/main/java/Kamen_Rider_Craft_4TH/item/build/Item_full_bottle.java
+++ src/main/java/Kamen_Rider_Craft_4TH/item/build/Item_full_bottle.java
@@ -40,7 +40,6 @@ public class Item_full_bottle extends Item implements IHasModel
 	public Item_full_bottle(int NUM ,String NUM2,String name)
 	{
 		super();
-		this.setHasSubtypes(true);
 		this.setMaxDamage(0);
 
 		num=NUM;
