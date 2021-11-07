24af887069f9db6e7a1e4eeea2dea46e3d78f859
kelcok22 <kelcok22@live.co.uk>
2021-11-07 22:34:56 +0000

    Update mobsCore.java

--- src/main/java/Kamen_Rider_Craft_4TH/mobsCore.java
+++ src/main/java/Kamen_Rider_Craft_4TH/mobsCore.java
@@ -78,6 +78,7 @@ import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_power_up_core;
 import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_red_nazca_dopant;
 import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_redyue;
 import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_roshuo;
+import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_shocker_riders;
 import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_storious;
 import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_taboo_dopant;
 import Kamen_Rider_Craft_4TH.mobs.Boss.Entity_uva;
@@ -221,6 +222,8 @@ public class mobsCore {
 		EntityRegistry.registerModEntity(new  ResourceLocation(Refercence.MODID+":ShockerCombatmanMob"),Entityshocker_combatman.class, "ShockerCombatmanMob", newMobID(), TokuCraft_core.instance, 80, 3, false, 000000,0xFFFFFF);
 		EntityRegistry.addSpawn(Entityshocker_combatman.class,  15, 1, 4, EnumCreatureType.MONSTER,Biome.getBiome(1),Biome.getBiome(2),Biome.getBiome(3),Biome.getBiome(4),Biome.getBiome(5),Biome.getBiome(6),Biome.getBiome(7),Biome.getBiome(10),Biome.getBiome(11),Biome.getBiome(12),Biome.getBiome(14),Biome.getBiome(15),Biome.getBiome(16),Biome.getBiome(17),Biome.getBiome(18),Biome.getBiome(19),Biome.getBiome(20),Biome.getBiome(21),Biome.getBiome(22),Biome.getBiome(23),Biome.getBiome(24),Biome.getBiome(25),Biome.getBiome(26),Biome.getBiome(27),Biome.getBiome(28),Biome.getBiome(29),Biome.getBiome(30),Biome.getBiome(31),Biome.getBiome(32),Biome.getBiome(33),Biome.getBiome(34),Biome.getBiome(35),Biome.getBiome(36),Biome.getBiome(37),Biome.getBiome(38),Biome.getBiome(39),Biome.getBiome(129),Biome.getBiome(130),Biome.getBiome(131),Biome.getBiome(132),Biome.getBiome(133),Biome.getBiome(134),Biome.getBiome(140),Biome.getBiome(149),Biome.getBiome(151),Biome.getBiome(155),Biome.getBiome(156),Biome.getBiome(157),Biome.getBiome(158),Biome.getBiome(160),Biome.getBiome(161),Biome.getBiome(162),Biome.getBiome(163),Biome.getBiome(164),Biome.getBiome(165),Biome.getBiome(166),Biome.getBiome(167));
 
+		EntityRegistry.registerModEntity(new  ResourceLocation(Refercence.MODID+":shocker_ridersMob"),Entity_shocker_riders.class, "shocker_ridersMob", newMobID(), TokuCraft_core.instance, 80, 3, false, 000000,0xFFFFFF);
+		
 		EntityRegistry.registerModEntity(new  ResourceLocation(Refercence.MODID+":DestronCombatmanMob"),Entitydestron_combatman.class, "DestronCombatmanMob", newMobID(), TokuCraft_core.instance, 80, 3, false, 000000,0xFFFFFF);
 		EntityRegistry.addSpawn(Entitydestron_combatman.class,  15, 1, 4, EnumCreatureType.MONSTER,Biome.getBiome(2),Biome.getBiome(3),Biome.getBiome(4),Biome.getBiome(6),Biome.getBiome(10)
 				,Biome.getBiome(28),Biome.getBiome(30),Biome.getBiome(31),Biome.getBiome(32),Biome.getBiome(33),Biome.getBiome(34),Biome.getBiome(39));
