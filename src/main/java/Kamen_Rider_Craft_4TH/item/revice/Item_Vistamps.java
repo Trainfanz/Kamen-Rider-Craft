394ecbbf601360a03a4ca9a30e6bf3bcf36d6a64
kelcok22 <kelcok22@live.co.uk>
2021-12-13 00:07:34 +0000

    Item_Vistamps update

--- src/main/java/Kamen_Rider_Craft_4TH/item/revice/Item_Vistamps.java
+++ src/main/java/Kamen_Rider_Craft_4TH/item/revice/Item_Vistamps.java
@@ -6,6 +6,7 @@ import Kamen_Rider_Craft_4TH.TokuCraft_core;
 import Kamen_Rider_Craft_4TH.blocks.hiden_3d_printing;
 import Kamen_Rider_Craft_4TH.blocks.zaia_3d_printing;
 import Kamen_Rider_Craft_4TH.blocks.wood.BlockHelheimVine;
+import Kamen_Rider_Craft_4TH.item.zero_one.Item_progrise_keys;
 import Kamen_Rider_Craft_4TH.util.IHasModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -26,6 +27,13 @@ public class Item_Vistamps extends Item implements IHasModel
 
 	public int setBase = 0;
 
+	public boolean CanReviUse = false;
+	public boolean CanViceUse = false;
+	public boolean CanLiveUse = false;
+	public boolean CanEvilUse = false;	
+	public boolean CanJeanneUse = false;
+	public boolean CanDemonsUse = false;
+	
 	public static String[] ARMS= new String[] {"base","eagle","mammoth","megalodon","ptera","lion","jackal","kong","kamakiri","brachio","kajiki"
 			,"bat","spider","batta","mogura","turtle","scorpion","condor","cobra","white_leo","quetzalcoatlus","neo_batta","","","","","","","","","",""
 			,"","","","","","","","","","","","","","","","","","","","","","","","","",""};
@@ -59,6 +67,42 @@ public class Item_Vistamps extends Item implements IHasModel
 		TokuCraft_core.proxy.registerItemRender(this,0,"inventory");
 	}
 
+	
+	public Item_Vistamps AddReviCanUse()
+	{
+		CanReviUse=true;
+		return this;
+	}
+	
+	public Item_Vistamps AddViceCanUse()
+	{
+		CanViceUse=true;
+		return this;
+	}
+	
+	public Item_Vistamps AddLiveCanUse()
+	{
+		CanLiveUse=true;
+		return this;
+	}
+	
+	public Item_Vistamps AddEvilCanUse()
+	{
+		CanEvilUse=true;
+		return this;
+	}
+	
+	public Item_Vistamps AddJeanneCanUse()
+	{
+		CanJeanneUse=true;
+		return this;
+	}
+	
+	public Item_Vistamps AddDemonsCanUse()
+	{
+		CanDemonsUse=true;
+		return this;
+	}
 	/**
 	 * Called when the equipped item is right clicked.
 	 */
@@ -70,9 +114,12 @@ public class Item_Vistamps extends Item implements IHasModel
 			if (playerIn.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() instanceof item_revicedriver){
 				item_revicedriver belt = (item_revicedriver) playerIn.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem();
 
+			
+					if (CanReviUse==true&belt.Rider=="revi"||CanViceUse==true&belt.Rider=="vice"){
+
 						item_revicedriver.set_lock(playerIn.getItemStackFromSlot(EntityEquipmentSlot.FEET),num2);
 						item_revicedriver.set_core(playerIn.getItemStackFromSlot(EntityEquipmentSlot.FEET),setBase, "1");			
-
+					}
 			}
 		}
 		playerIn.setActiveHand(handIn);
