704747ebd608e4e554eb5263900fc9fb77c22551
kelcok22 <kelcok22@live.co.uk>
2022-02-19 02:17:31 +0000

    Update item_revicedriver.java

--- src/main/java/Kamen_Rider_Craft_4TH/item/revice/item_revicedriver.java
+++ src/main/java/Kamen_Rider_Craft_4TH/item/revice/item_revicedriver.java
@@ -8,6 +8,8 @@ import Kamen_Rider_Craft_4TH.RiderItems;
 import Kamen_Rider_Craft_4TH.Tabs;
 import Kamen_Rider_Craft_4TH.TokuCraft_core;
 import Kamen_Rider_Craft_4TH.item.ooo.item_OOOdriver;
+import Kamen_Rider_Craft_4TH.item.zero_one.Item_progrise_keys;
+import Kamen_Rider_Craft_4TH.item.zero_one.item_zero_onedriver;
 import Kamen_Rider_Craft_4TH.model.Model_breaking_mammoth;
 import Kamen_Rider_Craft_4TH.model.model_belt_plus;
 import Kamen_Rider_Craft_4TH.potion.PotionCore;
@@ -139,6 +141,95 @@ public class item_revicedriver extends ItemArmor  implements IHasModel
 							if (player.getItemStackFromSlot(EntityEquipmentSlot.CHEST).getItem() == ReiwaRiderItems.revicetroso){
 								if (player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == ReiwaRiderItems.revicehead){	
 
+									String form = Item_Vistamps.ARMS[DRIVER];
+									int base = item_revicedriver.get_core(player.getItemStackFromSlot(EntityEquipmentSlot.FEET), "1");
+									
+									if (item_revicedriver.get_lockbase(player.getItemStackFromSlot(EntityEquipmentSlot.FEET))!="base") {
+										form = item_revicedriver.get_lockbase(player.getItemStackFromSlot(EntityEquipmentSlot.FEET));
+									}
+									
+									if (form=="eagle"){
+										player.addPotionEffect(new PotionEffect(PotionCore.FLY_POTION, 5, 0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 250,0,true,false));
+									}else if (form=="mammoth"){
+										player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 25,1,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 25,0,true,false));
+									}else if (form=="megalodon"){
+										if (player.isInWater()){
+											if (player.isSneaking()){
+											Vec3d look = player.getLookVec();
+											player.motionX=look.x/2;
+											player.motionY=look.y/2;
+											player.motionZ=look.z/2;
+										}}
+										player.addPotionEffect(new PotionEffect(MobEffects.WATER_BREATHING, 5, 0,true,false));
+									}else if (form=="ptera"){
+										player.addPotionEffect(new PotionEffect(PotionCore.FLY_POTION, 5, 0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 5,1,true,false));
+									}else if (form=="lion"){
+										player.addPotionEffect(new PotionEffect(PotionCore.PUNCH_BOOST_POTION, 5, 6,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 250,0,true,false));
+									}else if (form=="jackal"){
+										player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 5, 1,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 5, 2,true,false));
+									}else if (form=="kong"){
+										player.addPotionEffect(new PotionEffect(PotionCore.PUNCH_BOOST_POTION, 5, 5,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 25,0,true,false));
+									}else if (form=="kamakiri"){
+										player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 5, 1,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 5, 1,true,false));
+									}else if (form=="brachio"){
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 5, 1,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 5, 1,true,false));
+									}else if (form=="neo_batta"){
+										player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 5, 6,true,false));
+									}else if (form=="condor"){
+										player.addPotionEffect(new PotionEffect(PotionCore.FLY_POTION, 5, 0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 250,0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 250,2,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 250,3,true,false));
+									}else if (form=="kangaroo"){
+										player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 5, 3,true,false));
+										player.addPotionEffect(new PotionEffect(PotionCore.PUNCH_BOOST_POTION, 5, 6,true,false));
+									}else if (form=="quetzalcoatlus"){
+										player.addPotionEffect(new PotionEffect(PotionCore.FLY_POTION, 5, 0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 250,2,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 250,2,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 250,3,true,false));
+									}else if (form=="white_leo"){
+										player.addPotionEffect(new PotionEffect(PotionCore.PUNCH_BOOST_POTION, 5, 7,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 250,0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 5,2,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 5,3,true,false));
+									}
+									
+									if (player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == ReiwaRiderItems.revice_driver){
+										if (base==0){
+										player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 25,0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 250,0,true,false));
+										}else if (base==1){
+											player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 25,5,true,false));
+											player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 25,1,true,false));
+											player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 25,1,true,false));
+											player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 25,1,true,false));
+											
+										}
+									}else if (player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == ReiwaRiderItems.two_sidriver_evil){
+										player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 5,0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 5, 0,true,false));
+										
+									}else if (player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == ReiwaRiderItems.two_sidriver_live){
+										player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 5,0,true,false));
+										player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 5, 0,true,false));
+										
+									}else if (player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == ReiwaRiderItems.demons_driver){
+										
+										
+									}else if (player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == ReiwaRiderItems.libera_driver){
+										
+									}
+										
+									
 								}
 							}
 						}
