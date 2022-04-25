2c17e3bf4be94dd9509182b7467f56972fdd3ec0
kelcok22 <kelcok22@live.co.uk>
2022-04-25 15:53:33 +0100

    Update Model_giger.java

--- src/main/java/Kamen_Rider_Craft_4TH/model/Model_giger.java
+++ src/main/java/Kamen_Rider_Craft_4TH/model/Model_giger.java
@@ -307,7 +307,7 @@ public class Model_giger extends ModelBiped {
 												this.bipedRightArmBase.render(f5);
 												this.bipedLeftLeg.render(f5);
 												GlStateManager.popMatrix();
-			GlStateManager.popMatrix();
+
 	}
 	
 	public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity entityIn)
