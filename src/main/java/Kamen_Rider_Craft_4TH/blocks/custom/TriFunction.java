7f77b7904d7d2ca5d9ed5eedf8446a26f3c79258
LiasDan <liasdan22@gmail.com>
2023-05-14 22:31:59 +0800

    General Kenobi

--- src/main/java/Kamen_Rider_Craft_4TH/blocks/custom/TriFunction.java
+++
@@ -1,6 +0,0 @@
-package Kamen_Rider_Craft_4TH.blocks.custom;
-
-@FunctionalInterface
-public interface TriFunction<T, R, V, U> {
-    public U apply(T t, R r, V v);
-}
--- src/main/java/Kamen_Rider_Craft_4TH/util/IHasModel.java
+++
@@ -1,7 +0,0 @@
-package Kamen_Rider_Craft_4TH.util;
-
-public interface IHasModel 
-{
-	public void registerModels();
-	
-}
