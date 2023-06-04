e80ffd63d4222c7383047c8556f276f3ac2c897a
kelcok22 <kelcok22@live.co.uk>
2023-06-04 22:38:17 +0100

    work on w

--- src/main/java/com/kelco/kamenridercraft/Items/W_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/Items/W_Rider_Items.java
@@ -2,6 +2,7 @@ package com.kelco.kamenridercraft.Items;
 
 import com.kelco.kamenridercraft.KamenRiderCraftCore;
 import com.kelco.kamenridercraft.Items.rider_armor_base.RiderArmorItem;
+import com.kelco.kamenridercraft.Items.rider_armor_base.RiderDriverItem;
 import com.kelco.kamenridercraft.Items.rider_armor_base.RiderFormChangeItem;
 import com.kelco.kamenridercraft.Items.w.WDriverItem;
 
@@ -96,7 +97,7 @@ public class W_Rider_Items {
 	            		new MobEffectInstance(MobEffects.DIG_SPEED, 40, 1,true,false),
 	            		new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 40, 3,true,false),
 	            		new MobEffectInstance(MobEffects.REGENERATION, 40, 1,true,false))
-	            .alsoChange2ndSlot(JOKER_MEMORY.get()).AddToTabList(RiderTabs.W_TAB_ITEM));
+	            .alsoChange2ndSlot(JOKER_MEMORY.get()).ChangeModel("geo/cyclonehopper_wingsarmor.geo.json").AddToTabList(RiderTabs.W_TAB_ITEM));
 	
 	    public static final RegistryObject<Item> XTREME_ACCEL_MEMORY = ITEMS.register("xtreme_accel_memory",
 	            () -> new RiderFormChangeItem(new Item.Properties(),0,"_cyclone_xtreme_accel","w","wdriver_belt",
@@ -107,12 +108,37 @@ public class W_Rider_Items {
 	            		new MobEffectInstance(MobEffects.REGENERATION, 40, 1,true,false))
 	            .alsoChange2ndSlot(JOKER_MEMORY.get()).AddToTabList(RiderTabs.W_TAB_ITEM));
 	
+	    public static final RegistryObject<Item> SKULL_MEMORY = ITEMS.register("skull_memory",
+	            () -> new RiderFormChangeItem(new Item.Properties(),0,"","skull","lostdriver_belt",
+	            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false))
+	            .AddToTabList(RiderTabs.W_TAB_ITEM));
+	
+	    
+	    public static final RegistryObject<Item> T2_CYCLONE_MEMORY = ITEMS.register("t2_cyclone_memory",
+	            () -> new RiderFormChangeItem(new Item.Properties(),0,"","cyclone","lostdriver_belt_c",
+	            		new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false),
+	            		new MobEffectInstance(MobEffects.JUMP, 40, 0,true,false))
+	            .AddToTabList(RiderTabs.W_TAB_ITEM));
+	
+	    public static final RegistryObject<Item> T2_JOKER_MEMORY = ITEMS.register("t2_joker_memory",
+	            () -> new RiderFormChangeItem(new Item.Properties(),0,"","joker","lostdriver_belt",
+	            		new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 1,true,false))
+	            .ChangeSlot(2).AddToTabList(RiderTabs.W_TAB_ITEM));
+	    
 	    
 	    public static final RegistryObject<Item> WDRIVER = ITEMS.register("wdriver",
 		           () -> new WDriverItem(ArmorMaterials.DIAMOND,"w",CYCLONE_MEMORY ,WHELMET,WCHESTPLATE,WLEGGINGS , new Item.Properties())
 		           .Add_Extra_Base_Form_Items(JOKER_MEMORY).AddToTabList(RiderTabs.W_TAB_ITEM));
 
-	    
+	    public static final RegistryObject<Item> LOSTDRIVER_JOKER = ITEMS.register("lostdriver_joker",
+		           () -> new RiderDriverItem(ArmorMaterials.DIAMOND,"joker",T2_JOKER_MEMORY ,WHELMET,WCHESTPLATE,WLEGGINGS , new Item.Properties()).AddToTabList(RiderTabs.W_TAB_ITEM));
+
+	    public static final RegistryObject<Item> LOSTDRIVER_CYCLONE = ITEMS.register("lostdriver_cyclone",
+		           () -> new RiderDriverItem(ArmorMaterials.DIAMOND,"cyclone",T2_CYCLONE_MEMORY ,WHELMET,WCHESTPLATE,WLEGGINGS , new Item.Properties()).AddToTabList(RiderTabs.W_TAB_ITEM));
+
+	    public static final RegistryObject<Item> LOSTDRIVER_SKULL = ITEMS.register("lostdriver_skull",
+		           () -> new RiderDriverItem(ArmorMaterials.DIAMOND,"skull",SKULL_MEMORY ,WHELMET,WCHESTPLATE,WLEGGINGS , new Item.Properties()).AddToTabList(RiderTabs.W_TAB_ITEM));
+
 	    /**
 	    public static final RegistryObject<Item> HELMET = ITEMS.register("head",
 	            () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()).AddToTabList(RiderTabs._TAB_ITEM));
--- src/main/java/com/kelco/kamenridercraft/Items/w/WDriverItem.java
+++ src/main/java/com/kelco/kamenridercraft/Items/w/WDriverItem.java
@@ -40,10 +40,11 @@ public class WDriverItem extends RiderDriverItem{
 
 
 	public ResourceLocation getModelResource(ItemStack itemstack,RiderArmorItem animatable, EquipmentSlot slot) {
+		
 		if (slot!= EquipmentSlot.HEAD) {
 			return new ResourceLocation(KamenRiderCraftCore.MODID, "geo/rider_plusbelt.geo.json");
 		}else
-		return new ResourceLocation(KamenRiderCraftCore.MODID, "geo/ichigo.geo.json");
+		return super.getModelResource(itemstack, animatable, slot);
 	}
 	
 	public  boolean getPartsForSlot(EquipmentSlot currentSlot,String  part) {
