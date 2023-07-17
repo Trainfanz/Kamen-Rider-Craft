6f378b7ef0a4af049d83467fd6477279db0e9c0c
LiasDan <liasdan22@gmail.com>
2023-07-18 03:45:47 +0800

    Shocker has invaded your village

--- src/main/java/com/kelco/kamenridercraft/Blocks/BaseFacingBlock.java
+++ src/main/java/com/kelco/kamenridercraft/Blocks/BaseFacingBlock.java
@@ -3,8 +3,10 @@ package com.kelco.kamenridercraft.Blocks;
 
 import java.util.List;
 
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -17,7 +19,7 @@ public class BaseFacingBlock extends Block {
 
 	public BaseFacingBlock(Properties prop) {
 		super(prop);
-		
+	      this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	  protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_53681_) {
@@ -30,6 +32,10 @@ public class BaseFacingBlock extends Block {
 
 	     public PushReaction getPistonPushReaction(BlockState p_53683_) {
 	        return PushReaction.PUSH_ONLY;
+	     }
+
+	     public BlockState rotate(BlockState p_48722_, Rotation p_48723_) {
+	        return p_48722_.setValue(FACING, p_48723_.rotate(p_48722_.getValue(FACING)));
 	     }
 
 	
