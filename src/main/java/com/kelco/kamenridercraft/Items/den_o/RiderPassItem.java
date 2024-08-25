a1d536a90498d18a160b808c0561d39eb380d4e9
BuildTools <gypsytrinity@hotmail.com>
2024-08-24 21:44:50 -0700

    Fix Rider Pass suckage

--- src/main/java/com/kelco/kamenridercraft/Items/den_o/RiderPassItem.java
+++ src/main/java/com/kelco/kamenridercraft/Items/den_o/RiderPassItem.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import com.kelco.kamenridercraft.Items.rider_armor_base.BaseItem;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
@@ -18,10 +18,6 @@ import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.entity.EntityTypeTest;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.server.ServerLifecycleHooks;
 
 
@@ -32,18 +28,11 @@ public class RiderPassItem extends BaseItem {
 		super(properties);
 	}
 
-	public static void teleportToDimension(ServerLevel otherDim, LivingEntity entity, Vec3 loc) {
-		BlockPos destY = new BlockPos((int)loc.x, 61, (int)loc.z);
-
-        int tries = 0;
-        while ((otherDim.getBlockState(destY).getBlock() != Blocks.AIR) &&
-                !otherDim.getBlockState(destY).canBeReplaced(Fluids.WATER) &&
-                (otherDim.getBlockState(destY.below()).getBlock() != Blocks.AIR) &&
-                !otherDim.getBlockState(destY.below()).canBeReplaced(Fluids.WATER) && (tries < 25)) {
-            destY = destY.above(2);
-            tries++;
-        }
-	    entity.teleportTo(otherDim, loc.x, destY.getY(), loc.z, new HashSet<>(), 0, 0);
+	public static void teleportToDimension(ServerLevel otherDim, LivingEntity entity) {
+		if (entity.isPassenger()) entity.stopRiding();
+		entity.teleportTo(otherDim, entity.getX(), Mth.clamp(entity.getY(), (double)otherDim.getMinBuildHeight(), (double)(otherDim.getMinBuildHeight() + ((ServerLevel)otherDim).getLogicalHeight() - 1)), entity.getZ(), new HashSet<>(), 0, 0);
+		while (!otherDim.noCollision(entity) || otherDim.containsAnyLiquid(entity.getBoundingBox())) entity.teleportRelative(0.0, 2.0, 0.0);
+		entity.randomTeleport(entity.getX(), entity.getY(), entity.getZ(), false);
 	}
 
 	public InteractionResultHolder<ItemStack> use(Level p_41128_, Player p_41129_, InteractionHand p_41130_) {
@@ -56,11 +45,11 @@ public class RiderPassItem extends BaseItem {
 			List<TamableAnimal> nearbyAllies = p_41128_.getEntitiesOfClass(TamableAnimal.class, p_41129_.getBoundingBox().inflate(30), entity ->
 															(entity.getOwner() == p_41129_ && !entity.isOrderedToSit()));
 			if (p_41128_.dimension() == SANDS_OF_TIME) {
-				for (LivingEntity ally : nearbyAllies) teleportToDimension(Server.overworld(), ally, p_41129_.getPosition(0));
-				teleportToDimension(Server.overworld(), p_41129_, p_41129_.getPosition(0));
+				for (LivingEntity ally : nearbyAllies) teleportToDimension(Server.overworld(), ally);
+				teleportToDimension(Server.overworld(), p_41129_);
 			} else {
-				for (LivingEntity ally : nearbyAllies) teleportToDimension(Server.getLevel(SANDS_OF_TIME), ally, p_41129_.getPosition(0));
-				teleportToDimension(Server.getLevel(SANDS_OF_TIME), p_41129_, p_41129_.getPosition(0));
+				for (LivingEntity ally : nearbyAllies) teleportToDimension(Server.getLevel(SANDS_OF_TIME), ally);
+				teleportToDimension(Server.getLevel(SANDS_OF_TIME), p_41129_);
 			}
 			if (itemstack.isDamageableItem()) itemstack.hurtAndBreak(1, p_41129_, player -> player.broadcastBreakEvent(player.getUsedItemHand()));
 			p_41129_.getCooldowns().addCooldown(this, 500);
