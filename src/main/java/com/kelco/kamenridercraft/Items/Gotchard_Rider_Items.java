6dd7c751b1ebf2306f930ef24c76140cc5bad437
LiasDan <liasdan22@gmail.com>
2024-09-08 13:26:36 +0800

    Legend Gaim

--- src/main/java/com/kelco/kamenridercraft/Items/Gotchard_Rider_Items.java
+++ src/main/java/com/kelco/kamenridercraft/Items/Gotchard_Rider_Items.java
@@ -277,6 +277,12 @@ public class Gotchard_Rider_Items {
      * donposeidon
      * dragonalos
      */
+	   
+    public static final RegistryObject<Item> LEGEND_RIDE_CHEMY_CARD = ITEMS.register("legend_ride_chemy_card",
+    		() -> new RiderFormChangeItem(new Item.Properties(),0,"","legend","legendriver_belt",
+    				new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 40, 1,true,false),
+    				new MobEffectInstance(MobEffects.DAMAGE_BOOST, 40, 0,true,false),
+    				new MobEffectInstance(MobEffects.JUMP, 40, 2,true,false)).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM));
     
 	public static final RegistryObject<Item> GOTCHARD_HELMET = ITEMS.register("gotchard_head",
             () -> new RiderArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM).ChangeRepairItem(BLANK_RIDE_CHEMY_CARD.get()));
@@ -292,7 +298,10 @@ public class Gotchard_Rider_Items {
     public static final RegistryObject<Item> VALVARADRAW_BUCKLE = ITEMS.register("valvaradraw_buckle",
     		() -> new ValvaradItem(ArmorMaterials.DIAMOND,"valvarad", MADWHEEL_RIDE_CHEMY_CARD ,GOTCHARD_HELMET, GOTCHARD_CHESTPLATE,GOTCHARD_LEGGINGS , new Item.Properties())
     		.AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM).ChangeRepairItem(BLANK_RIDE_CHEMY_CARD.get()));
-
+    
+    public static final RegistryObject<Item> LEGENDRIVER = ITEMS.register("legendriver",
+    		() -> new RiderDriverItem(ArmorMaterials.DIAMOND,"legend", LEGEND_RIDE_CHEMY_CARD ,GOTCHARD_HELMET, GOTCHARD_CHESTPLATE,GOTCHARD_LEGGINGS , new Item.Properties()).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM).ChangeRepairItem(BLANK_RIDE_CHEMY_CARD.get()));
+    
     
 	public static final RegistryObject<BaseBlasterItem> GOTCHARGE_GUN = ITEMS.register("gotcharge_gun",
 			() -> new BaseBlasterItem(Tiers.DIAMOND, 3, -2.4F, new Item.Properties()).AddToTabList(RiderTabs.GOTCHARD_TAB_ITEM).ChangeRepairItem(BLANK_RIDE_CHEMY_CARD.get()));
